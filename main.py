e7b642d7e15a89f796c392d6834fab66ea32ee07
Ilya K <131490186+umphmeow@users.noreply.github.com>
2023-05-16 12:32:14 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,27 @@
 import re
 
 dict = {'1': 'один', '2': 'два', '3': 'три', '4': 'четыре', '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять', '0': 'ноль', '.': 'целых', ',': 'десятых'}
+print("Задание: Программа,которая читая файл, распознает, преобразует и выводит на экран числа по определенному правилу. Числа распознаются по законам грамматики русского языка. На ввод даются натуральные числа, не превышающие 1 000 000, у которых первые две цифры равны 77. Программа выводит на экран числа, без этих 7; Вычисляет среднее число между минимальным и максимальным и выводит прописью.")
+try:
+    with open('text.txt', 'r') as f_in:
+        data = f_in.read()
+        digits = [d.replace('77', '', 1) for d in re.findall(r'\b77\d*\b', data)]
+        d_array = [d for d in digits if d]
+        d_arr_int = list(map(int, d_array))
 
-with open('text.txt', 'r') as f_in:
-    data = f_in.read()
-    #проверка с помощью регулярных выражений
-    digits = [d.replace('77', '', 1) for d in re.findall(r'\b77\d*\b', data)]
-    d_array = [d for d in digits if d]
-    d_arr_int = list(map(int, d_array))
-
-if not d_array:
-    print('Не найдено цифр удовлетворяющих заданию или цифр нет вообще')
-else:
-    print('Массив чисел:', d_arr_int)
-    max_d = max(d_arr_int)
-    min_d = min(d_arr_int)
-    print('Максимальное число:', max_d)
-    print('Минимальное число:', min_d)
-    answer = (max_d + min_d) / 2
-    print('Среднее арифметическое:', answer)
-    answer_txt = ''
-    for digit in str(answer):
-        answer_txt += ' ' + dict[digit]
-    print('Ответ:', answer_txt, dict[','])
+    if not d_array:
+        print('Не найдено цифр удовлетворяющих заданию или цифр нет вообще')
+    else:
+        print('Массив чисел:', d_arr_int)
+        max_d = max(d_arr_int)
+        min_d = min(d_arr_int)
+        print('Максимальное число:', max_d)
+        print('Минимальное число:', min_d)
+        answer = (max_d + min_d) / 2
+        print('Среднее арифметическое:', answer)
+        answer_txt = ''
+        for digit in str(answer):
+            answer_txt += ' ' + dict[digit]
+        print('Ответ:', answer_txt, dict[','])
+except FileNotFoundError:
+    print('Файл text.txt не найден')
